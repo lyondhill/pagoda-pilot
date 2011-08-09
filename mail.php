981ae4adf33253ae50642c201a2a5adc7b622b4e
lyondhill <lyondhill@gmail.com>
2011-08-09 16:59:48 -0600

    email lyon instead

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
 
-mail('tylerflint@gmail.com', 'Test Subject', "This is an email message from the pongo app");
+mail('lyondhill@gmail.com', 'Test Subject', "This is an email message from the pongo app");
 
 echo "email sent";
